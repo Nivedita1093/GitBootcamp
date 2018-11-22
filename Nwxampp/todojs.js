7939ffc57c541e634a37e031e20551e132f0bfbc
Nivedita Wasson <nivedita.wasson@cybergindia.local>
2018-11-22 23:51:40 +0530

    final todo changes

--- Nwxampp/todojs.js
+++ Nwxampp/todojs.js
@@ -2,6 +2,7 @@ window.onload=function f1()
 {
 get_from_db();
 get_taskcategory();
+get_taskcategory_select();
 input = document.getElementById("myInput");
 input.addEventListener("keyup", function(event)
   {   
@@ -14,6 +15,9 @@ input.addEventListener("keyup", function(event)
 };
 
 
+var tsk;
+var tsk_cat;
+
 function get_taskcategory()       //for the dropdown menu
 {
 $.ajax({
@@ -22,25 +26,141 @@ $.ajax({
           async:true, 
           dataType:"json",
           crossDomain:true,
-           success: function(response) 
+          success: function(response) 
           {
             for(var i=0;i<response.length;i++)
             {
-              var li=response[i].ToDo_Category;
+              tsk=response[i].ToDo_CategoryID;
+              tsk_cat=response[i].ToDo_Category;
               var abc=document.createElement('option');
-              abc.innerHTML=li;
-              abc.value=li;
+              abc.id="tskid_"+tsk;
+              abc.setAttribute("class","dropclass");
+              abc.innerHTML=tsk_cat;
+              abc.value=tsk_cat;
               document.getElementById("mydropdown").appendChild(abc);
             }
-
-          }
-          
-          
+          }     
       });
 }
 
 
-//var t
+
+
+
+
+var tsk1;
+var tsk_cat1;
+
+function get_taskcategory_select()       //for the dropdown menu select
+{
+$.ajax({
+          type: "GET",
+          url: 'http://localhost:50898/api/ToDoCategories',
+          async:true, 
+          dataType:"json",
+          crossDomain:true,
+          success: function(response) 
+          {
+            for(var i=0;i<response.length;i++)
+            {
+              tsk1=response[i].ToDo_CategoryID;
+              tsk_cat1=response[i].ToDo_Category;
+              var abc=document.createElement('option');
+              abc.id="tskid_t"+tsk1;
+              abc.setAttribute("class","select_dropclass");
+              abc.innerHTML=tsk_cat1;
+              abc.value=tsk_cat1;
+              document.getElementById("select_dropdown").appendChild(abc);
+            }
+          }     
+      });
+}
+
+
+var drp_val;
+var sel_cat;
+function sel_drp()
+{
+drp_val=document.getElementById("select_dropdown").value;
+//console.log(drp_val);
+if(drp_val=="Daily Task")
+{
+sel_cat=1;
+}
+else if(drp_val=="Weekly Task")
+{
+sel_cat=2;
+}
+}
+
+
+
+function tasks()     // to display daily and weekly tasks
+{
+var m=document.getElementById("mydropdown").value;
+//console.log(m);
+    $.ajax({
+          type: "GET",
+          url: 'http://localhost:50898/api/mytodolist/get',
+          async:true, 
+          dataType:"json",
+          crossDomain:true,
+          success: function(response) 
+          {
+            for(var i=0;i<response.length;i++)
+            {
+              var id_todo=response[i].ToDo_ID;
+              var stat_todo=response[i].ToDo_Status;
+              tsk=response[i].Category_ID;
+              //console.log(stat_todo);
+              if(m=="Daily Task")
+              {
+              if(tsk==1)
+                {
+                  while (parentdiv.hasChildNodes())
+                  {
+                  parentdiv.removeChild(parentdiv.lastChild);
+                  }
+                  //parentdiv.remove();
+                  var todo_div = document.createElement('div');
+                  todo_div.id=id_todo;
+                  todo_div.setAttribute("class","divclass");
+                  todo_div.innerHTML= '<span id=values_'+id_todo+'>'+response[i].ToDo_Description+'</span>';
+                  document.getElementById("parentdiv").appendChild(todo_div);
+
+                }
+              }
+              else if(m=="Weekly Task")
+              {
+              if(tsk==2)
+                {
+                  while (parentdiv.hasChildNodes())
+                  {
+                  parentdiv.removeChild(parentdiv.lastChild);
+                  }
+                  var todo_div = document.createElement('div');
+                  todo_div.id=id_todo;
+                  todo_div.setAttribute("class","divclass");
+                  todo_div.innerHTML= '<span id=values_'+id_todo+'>'+response[i].ToDo_Description+'</span>';
+                  document.getElementById("parentdiv").appendChild(todo_div);
+                }
+              }
+              else
+              {
+              location.reload();
+              }
+            
+            }
+          }     
+      });
+  
+}
+
+
+
+var db_id;
+var stat;
+
 function get_from_db()  //get from database on reload
 {
 $.ajax({
@@ -53,14 +173,14 @@ $.ajax({
           {
             for(var i=0;i<response.length;i++)
             {
-            var db_id=response[i].ToDo_ID;
-            var stat=response[i].ToDo_Status;
+            db_id=response[i].ToDo_ID;
+            stat=response[i].ToDo_Status;
             //var dynamicdivid=db_id;
             var tododiv = document.createElement('div');
             //tododiv.id=dynamicdivid;
             tododiv.id=db_id;
             tododiv.setAttribute("class","divclass");
-            tododiv.innerHTML= '<span id=values_'+db_id+'>'+response[i].ToDo_Description+'</span><span class="btn-span"><button onclick="statusElement('+db_id+')" class="statusbutton">Done</button><button onclick="editElement('+db_id+')" class="editbutton">Edit</button><button onclick="delElement('+db_id+')" class="delbutton">Delete</button>  </span>';
+            tododiv.innerHTML= '<span  id=values_'+db_id+'>'+response[i].ToDo_Description+'</span><span class="btn-span"><button onclick="statusElement('+db_id+')" class="statusbutton">Done</button><button onclick="editElement('+db_id+')" class="editbutton">Edit</button><button onclick="delElement('+db_id+')" class="delbutton">Delete</button>  </span>';
             document.getElementById("parentdiv").appendChild(tododiv);
             if(stat==0)
               {
@@ -98,12 +218,13 @@ function delElement(div_id)   //delete an element by id
  }
 
 
+
+
+
 var flag=0;
 function newElement() //used for new element creation and also called when edit element
 {
-
-//var date=new Date();
-
+var date=new Date();
  if(flag==0)
  {
   var inputValue = document.getElementById("myInput").value;
@@ -113,7 +234,7 @@ function newElement() //used for new element creation and also called when edit 
     }
   else
     {
-    post(inputValue);
+    post(inputValue,date);
     location.reload();
     document.getElementById("myInput").value = "";
     }
@@ -127,17 +248,18 @@ function newElement() //used for new element creation and also called when edit 
  }
 }
 
-//date_val.toISOString()
 
 
-function post(to_do_des)    //used to post values at new element creation
+
+function post(to_do_des,date_val)    //used to post values at new element creation
       {
+        console.log(sel_cat);
         $.ajax({
           type: "POST",
           url: 'http://localhost:50898/api/mytodolist/post',
           async:true, 
           dataType:"json",
-          data:{"ToDo_Description":to_do_des,"ToDo_Status": "0","CreateDate": "2018-11-05T00:00:00","Category_ID":"1"},
+          data:{"ToDo_Description":to_do_des,"ToDo_Status": "0","CreateDate":date_val.toISOString(),"Category_ID":sel_cat},
           crossDomain:true,
           success: function(response) 
           {
@@ -165,20 +287,42 @@ function editElement(div_id)    //called on click of edit element
 
 function edit_ele(div_id,to_do_change)   //used to put values at edit element
 { 
-     $.ajax({
+
+ $.ajax ({
+          type: "GET",
+          url: 'http://localhost:50898/api/ToDoes/'+div_id,
+          async:true, 
+          dataType:"json",
+          crossDomain:true,
+           success: function(response) 
+          {
+            
+            stat_id=response.ToDo_ID;
+           // todo_des=response.ToDo_Description;
+            todo_status=response.ToDo_Status;
+            datecreate=response.CreateDate;
+            categoryid=response.Category_ID;
+            
+
+          $.ajax({
           type:"PUT",
           url:'http://localhost:50898/api/ToDoes/'+div_id,
           async:true, 
           dataType:"json",
           crossDomain:true,
-          data:{"ToDo_ID":div_id,"ToDo_Description":to_do_change,"ToDo_Status": "0","CreateDate": "2018-11-05T00:00:00","Category_ID":"1"},
+          data:{"ToDo_ID":stat_id,"ToDo_Description":to_do_change,"ToDo_Status":todo_status,"CreateDate":datecreate,"Category_ID":categoryid},
           success: function(response)
           {
             document.getElementById(gbid).innerHTML=to_do_change;
             // alert("Your content has been edited");
             // location.reload();
           }
-        });
+          });
+
+          }
+          });
+
+
 }
 
 
@@ -245,3 +389,6 @@ function statusElement(div_id)  //to change complete/incomplete status of a task
           });
 
 }
+
+
+
